3bf4ea040b7077ce9b2835d53e36cc3154c634a5
Yakovleva <AYakovleva@kiev.luxoft.com>
2017-07-12 19:21:08 +0300

    Some changes were made

--- task1/hello1.py
+++
@@ -1,4 +0,0 @@
-def echo(stri,x):
-   return stri * x
-
-print(echo('foo', 5))
--- task1/run-cmd.py
+++ task1/run-cmd.py
@@ -1,33 +1,48 @@
 """import subprocess
-
 cmdout = subprocess.Popen("dir", shell = True, stdout = subprocess.PIPE).stdout.read()
-
 print(cmdout)
+
+text = '5'
+proc = subprocess.Popen(
+    './double',stdout=subprocess.PIPE,
+    stdin=subprocess.PIPE)
+proc.stdin.write(text.encode())
+proc.stdin.close()
+result = proc.stdout.read()
+print (result)
+proc.wait()
 """
 
 import subprocess
 
-text = '1'
-proc = subprocess.Popen("prog1", 
+def funcRead(x):
+        proc = subprocess.Popen("prog1", 
                         stdout = subprocess.PIPE,
                         stdin = subprocess.PIPE)
-proc.stdin.write(text.encode())
-proc.stdin.close()
-cmdout = proc.stdout.read()
+        
+        proc.stdin.write(text[num].encode())
+        proc.stdin.close()
+        cmdout = proc.stdout.read()
 
-print(cmdout)
-proc.wait()
-"""
-for(i = '1'; i < '5'; i++) {
-    proc = subprocess.Popen("prog1", 
-                        stdout = subprocess.PIPE,
-                        stdin = subprocess.PIPE)
-    proc.stdin.write(i.encode())
-    proc.stdin.close()
-    cmdout = proc.stdout.read()
+        file.write(text[num] +'\t' + cmdout.decode() + '\n')
+        return cmdout
 
-    print("cmdout ")
-    proc.wait()
+
+errors = 0
+i=0
+text = ['1', '2', '5']
+file = open('report.txt', 'w')
+while (i != 10):
+    num = 0
+    while (num < 3):
+        cmdout = funcRead(num)        
+
+        print(cmdout)
+        if (cmdout == b'toto'):
+            errors += 1
+        num += 1
     
-}
-"""
+    i += 1
+file.write('\n' + 'Error\'s amount\n' + str(errors))    
+file.close()
+
